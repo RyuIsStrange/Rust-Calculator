2209bf2b68d6cee28fdbfa47a82713fc4a7ecd78
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2025-01-16 23:02:43 -0600

    feat: loop this

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{io, num::ParseIntError, process};
+use core::time;
+use std::{io, num::ParseIntError, process, thread::sleep};
 
 #[derive(Debug)]
 enum Error {
@@ -39,36 +40,62 @@ fn calc(v1: i64, op: &str, v2: i64) -> Result<i64, Error> {
 }
 
 fn main() {    
-    println!("Enter your input in the format: <number> <operator> <number>");
-    let mut input = String::new();
+    loop {
+        println!("============================================================");
+        println!("Enter your input in the format: <number> <operator> <number>");
+        println!("============================================================");
+        println!("    Or you may input 'exit' to stop using the calculator    ");
+        println!("============================================================");
 
-    io::stdin().read_line(&mut input).expect("Failed to read input");
+        let mut input = String::new();
 
-    let parts: Vec<&str> = input.trim().split_whitespace().collect();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
 
-    if parts.len() != 3 {
-        eprintln!("Invalid input format. Use: <number> <operator> <number>");
-        process::exit(1); // Punish the user
-    }
+        let parts: Vec<&str> = input.trim().split_whitespace().collect();
 
-    let val1 = parts[0];
-    let opr = parts[1];
-    let val2 = parts[2];
-
-    let ints = intmaxxing(val1, val2);
-    
-    if let (Ok(int1), Ok(int2)) = ints {
-        match calc(int1, opr.trim(), int2) {
-            Ok(res) => println!("Result: {}", res),
-            Err(e) => eprintln!("Error in calculation {:?}", e)
+        if parts.len() == 1 {
+            if parts[0] == "exit" {
+                print!("Bye bye\n");
+                process::exit(1); // Grant the user the sweet mercy of death
+            }
         }
-    } else {
-        if let Err(e1) = ints.0 {
-            eprintln!("Error parsing first value: {}", e1);
+
+        if parts.len() != 3 {
+            eprintln!("Invalid input format. Use: <number> <operator> <number>");
+            process::exit(1); // Punish the user
         }
-        if let Err(e2) = ints.1 {
-            eprintln!("Error parsing second value: {}", e2);
+
+        let val1 = parts[0];
+        let opr = parts[1];
+        let val2 = parts[2];
+
+        let ints = intmaxxing(val1, val2);
+        
+        if let (Ok(int1), Ok(int2)) = ints {
+            match calc(int1, opr.trim(), int2) {
+                Ok(res) => {
+                    println!("Result: {}\n", res);
+                    sleep(time::Duration::from_secs(1));
+                }
+                Err(e) => {
+                    eprintln!("Error in calculation {:?}\n", e);
+                    sleep(time::Duration::from_secs(1))
+                }
+            }
+        } else {
+            if let Err(e1) = ints.0 {
+                eprintln!("Error parsing first value: {}\n", e1);
+                sleep(time::Duration::from_secs(1));
+            }
+            if let Err(e2) = ints.1 {
+                eprintln!("Error parsing second value: {}\n", e2);
+                sleep(time::Duration::from_secs(1));
+            }
+            /*
+            Ok maybe dont punish them for now
+
+            process::exit(1); // Punish the user
+            */
         }
-        process::exit(1); // Punish the user
     }
 }
